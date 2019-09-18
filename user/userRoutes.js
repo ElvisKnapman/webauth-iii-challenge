88ca0511f91e96b645bce64a9a236d7438d20a51
Elvis Knapman <elvis@elvisknapman.com>
2019-09-18 18:31:19 -0400

    :sparkles: added route for getting users

---
+++ user/userRoutes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+
+const Users = require("../user/user-model.js");
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await Users.getUsers();
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(400).json({ message: "You are not authorized" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server could not retrieve users" });
+  }
+});
+
+module.exports = router;
