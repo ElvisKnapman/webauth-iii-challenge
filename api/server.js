c359069dc3f5af80174ed59224d7108c731ea879
Elvis Knapman <elvis@elvisknapman.com>
2019-09-18 18:29:31 -0400

    :sparkles: added routes

--- api/server.js
+++ api/server.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const server = express();
 
+// routes
+const authRoutes = require("../auth/authRoutes.js");
+const userRoutes = require("../user/userRoutes.js");
+
+server.use(express.json());
+
+server.use("/api/auth", authRoutes);
+server.use("/api/user", userRoutes);
+
 server.get("/", (req, res) => {
   res.status(200).json({ message: "API up..." });
 });
